c32d07b797c26bf733c5f89ed92a913dbe2a52f8
Ben Lonc <bnlonc@gmail.com>
2023-03-03 00:59:22 -0800

    fixed top level and expanded assembler

--- assembler/Assembler/src/Lib.hs
+++ assembler/Assembler/src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib
-    ( assemble
+    ( 
+        assemble,
+        unrollMultiAddI
     ) where
 
 import Data.Char(intToDigit)
@@ -10,10 +12,11 @@ type InstrWord              = String
 type Instruction            = (InstrWord, InstrWord, InstrWord, InstrWord)
 
 assemble :: String -> MachineCode
-assemble instrs = intercalate "\n" (map encodeInstruction (resolveMacros (parseInstructions instrs)))
+assemble instrs = intercalate "\n" (map encodeInstruction (resolveMacros (parseInstructionString instrs)))
 
-parseInstructions :: String -> [Instruction]
-parseInstructions str = map splitLine (lines str)
+-- Convert the long, \n-delimeted string of instructions to a list of instruction tuples 
+parseInstructionString :: String -> [Instruction]
+parseInstructionString str = map splitLine (lines str)
     where 
         splitLine :: String -> Instruction 
         splitLine str = packageInstr (words str)
@@ -23,21 +26,35 @@ parseInstructions str = map splitLine (lines str)
                 packageInstr (name:reg1:reg2:[])        = (name, reg1, reg2, "")
                 packageInstr (name:reg1:reg2:count:[])  = (name, reg1, reg2, count)
 
-
+-- Traverse the list of instructions and resolve every macro to its expanded form in base assembly language 
 resolveMacros :: [Instruction] -> [Instruction]
 resolveMacros []     = []; 
 resolveMacros (l:ls) = (resolveInstr l) ++ (resolveMacros ls)
     where     
         resolveInstr :: Instruction -> [Instruction]
-        resolveInstr (name,     arg1,  arg2,    n)  | (any (name ==) ["RShift", "LShift", "Rotate", "DPRSh", "DPLSh"]) = unrollMultiMacro (name, arg1, arg2, n)
-        resolveInstr ("Zero",   reg,   _,       _)  = [("Xor", reg, reg, "")]
-        resolveInstr ("Set",    reg,   value,   _)  = [("Xor", reg, reg, ""), ("AddI", reg, value, "")]
-        resolveInstr ("Swap",   reg1,  reg2,    _)  = [("Xor", reg1, reg2, ""), ("Xor", reg2, reg1, ""), ("Xor", reg1, reg2, "")]
-        resolveInstr ("Copy",   reg1,  reg2,    _)  = [("Xor", reg1, reg1, ""), ("Add", reg1, reg2, "")]
+        resolveInstr (name,     arg1,   arg2,   n)  | (any (name ==) ["RShift", "LShift", "Rotate", "DPRSh", "DPLSh"]) = unrollMultiShift (name, arg1, arg2, n)
+        resolveInstr ("Zero",   reg,    _,      _)  = [("Xor", reg, reg, "")]
+        resolveInstr ("Set",    reg,    value,  _)  = [("Xor", reg, reg, ""), ("AddI", reg, value, "")]
+        resolveInstr ("Swap",   reg1,   reg2,   _)  = [("Xor", reg1, reg2, ""), ("Xor", reg2, reg1, ""), ("Xor", reg1, reg2, "")]
+        resolveInstr ("Copy",   reg1,   reg2,   _)  = [("Xor", reg1, reg1, ""), ("Add", reg1, reg2, "")]
+        resolveInstr ("AddI",   reg,    value,  _)  = unrollMultiAddI reg value
         resolveInstr instr                          = [instr]
 
-unrollMultiMacro :: Instruction -> [Instruction]
-unrollMultiMacro (name, reg1, reg2, count)    | any (name ==) ["DPRSh", "DPLSh"]    = expandDoublePrecision ( take (read count::Int) (repeat (name, reg1, reg2, "")) )
+unrollMultiAddI :: InstrWord -> InstrWord -> [Instruction]
+unrollMultiAddI reg val@(v:vs)  | v == '-'  = reverse (unrollNegative reg (read val::Int))
+                                | otherwise = reverse (unrollPositive reg (read val::Int))
+    where 
+        unrollPositive :: String -> Int -> [Instruction]
+        unrollPositive reg val  | val <= 7          = [("AddI", reg, (show val), "")]
+                                | mod val 2 == 0    = ("LShift", reg, "", ""):(unrollPositive reg (div val 2))
+                                | otherwise         = ("AddI", reg, "7", ""):(unrollPositive reg (val - 7))
+        unrollNegative :: String -> Int -> [Instruction] 
+        unrollNegative reg val  | val >= -8 = [("AddI", reg, (show val), "")]
+                                | otherwise = ("AddI", reg, "-8", ""):(unrollNegative reg (val + 8))
+
+-- Unroll all shift macros to multiple of their base assembly single-shift equivalents
+unrollMultiShift :: Instruction -> [Instruction]
+unrollMultiShift (name, reg1, reg2, count)    | any (name ==) ["DPRSh", "DPLSh"]    = expandDoublePrecision ( take (read count::Int) (repeat (name, reg1, reg2, "")) )
     where 
         expandDoublePrecision :: [Instruction] -> [Instruction]
         expandDoublePrecision [] = []
@@ -46,16 +63,16 @@ unrollMultiMacro (name, reg1, reg2, count)    | any (name ==) ["DPRSh", "DPLSh"]
                 companionShiftName :: String -> String 
                 companionShiftName "DPRSh" = "RShift"
                 companionShiftName _       = "LShift"
-unrollMultiMacro (name, reg, count, _)                                              = take (read count::Int) (repeat (name, reg, "", ""))
+unrollMultiShift (name, reg, count, _)                                              = take (read count::Int) (repeat (name, reg, "", ""))
 
-
+-- Convert assembly instructions into binary machine code 
 encodeInstruction :: Instruction -> MachineCode
 encodeInstruction (name, reg1, reg2, _) | name == "Add"     = "000" ++ (encodeRegAddr reg1) ++ (encodeRegAddr reg2)
                                         | name == "Xor"     = "001" ++ (encodeRegAddr reg1) ++ (encodeRegAddr reg2)
                                         | name == "And"     = "010" ++ (encodeRegAddr reg1) ++ (encodeRegAddr reg2)
                                         | name == "Load"    = "011" ++ (encodeTruncRegAddr reg1) ++ ('0':(encodeRegAddr reg2))
                                         | name == "Store"   = "011" ++ (encodeTruncRegAddr reg1) ++ ('1':(encodeRegAddr reg2))
-encodeInstruction (name, reg, imm, _)   | name == "AddI"    = "100" ++ (encodeTruncRegAddr reg) ++ (getFourBitBinary imm)
+encodeInstruction (name, reg, imm, _)   | name == "AddI"    = "100" ++ (encodeTruncRegAddr reg) ++ (toBinary 4 imm)
 encodeInstruction (name, reg, _, _)     | name == "LShift"  = "101" ++ (encodeRegAddr reg) ++ "000"
                                         | name == "RShift"  = "101" ++ (encodeRegAddr reg) ++ "010"
                                         | name == "Rotate"  = "101" ++ (encodeRegAddr reg) ++ "011"
@@ -84,27 +101,21 @@ encodeBranchMode "IfNotZeroAbs"        = "101"
 encodeBranchMode "IfNotNegativeReg"    = "110"
 encodeBranchMode "IfNotNegativeAbs"    = "111"
 
-getFourBitBinary :: String -> String
-getFourBitBinary n = getFourBitBinaryHelper (read n::Int)
-    where 
-        getFourBitBinaryHelper n    | n >= 0    = '0':(getThreeBitBinary n)
-                                    | otherwise = bin (16 + n) 
-
+-- Convert register names to their truncated binary forms, like "5" to "01"
 encodeTruncRegAddr :: String -> String 
 encodeTruncRegAddr str = tail (encodeRegAddr str)
 
+-- Convert register names like "r5" or short immediates like "5" into their three-bit binary form, "101"
 encodeRegAddr :: String -> String 
-encodeRegAddr (r:s:[]) | r == 'r'  = getThreeBitBinary (read [s]::Int)
-encodeRegAddr (r:[])               = getThreeBitBinary (read [r]::Int)
+encodeRegAddr (r:s:[]) | r == 'r'  = toBinary 3 [s]
+encodeRegAddr (r:[])               = toBinary 3 [r]
 encodeRegAddr _                    = ""
 
-getThreeBitBinary :: Int -> String
-getThreeBitBinary n | n <= 1    = '0':'0':(bin n)
-                    | n <= 3    = '0':(bin n)
-                    | otherwise = (bin n)
-
-bin :: Int -> String
-bin = reverse . go
-    where go 0 = "0"
-          go 1 = "1"
-          go n = intToDigit (mod n 2) : go (div n 2)
+-- Convert numerical strings into their n-bit binary forms
+toBinary :: Int -> String -> String 
+toBinary n str = toBinaryHelper n (read str::Int)
+    where 
+        toBinaryHelper :: Int -> Int -> String 
+        toBinaryHelper 0    _   = ""
+        toBinaryHelper bits num | num >= (2 ^ (bits - 1)) = '1':(toBinaryHelper (bits - 1) (num - (2 ^ (bits - 1))))
+                                | otherwise              = '0':(toBinaryHelper (bits - 1) num)
