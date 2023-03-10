0ac13d2daf47a191885ed60b6bd1f2d75eff7467
Ben Lonc <bnlonc@gmail.com>
2023-03-10 02:57:23 -0800

    Bug fixes

--- prog1_tb.sv
+++ prog1_tb.sv
@@ -18,6 +18,7 @@ bit  [11:1] d1_in[15];           // original messages
 bit         p0, p8, p4, p2, p1;  // Hamming block parity bits
 bit  [15:0] d1_out[15];          // orig messages w/ parity inserted
 bit  [15:0] score1, case1;
+integer     seed;
 
 // your device goes here
 // change "top_level" if you called your device something different
